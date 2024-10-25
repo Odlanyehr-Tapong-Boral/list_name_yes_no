bcb07be35440d7733c1b0fe29e3fb35c0e27e7c0
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-25 17:16:29 +0800

    yes/no option

--- listname_yes_no.py
+++ listname_yes_no.py
@@ -16,12 +16,43 @@ while True:
     
 data = []
 data.append(("name", var1))
-data.append(("age - {var2}"))
+data.append(("age", var2))
 
 print(data)
-
-
-
 # Store all the collected information into array. After every input, ask the user if want to input another entry.
+var3 = (input("Add another entry? "))
+# When “Yes”, ask the user again for input. Do it until the user respond “No”
+
+if var3 == 'yes' or var3 == 'Yes':
+    try:
+        var4 =(input("Please input name "))
+    except:
+        print("Try again")
+        
+    while True:
+        try:
+            var5 = int(input("Please input age "))
+            break
+        except:
+            print("Try again")
+            
+            
+            
+    data = []
+    data.append(("name", var4))
+    data.append(("age", var5)) 
+
+    print(data)
+ #When the user responded “No”, display the name and age of the oldest person.
+else:
+    print(f"Oldest record is", data)
+                        
+ 
+
+   
+ 
+
+
+
 # When “Yes”, ask the user again for input. Do it until the user respond “No”. When the user responded “No”, display the name and age of the oldest person.
 # Use the array in checking who is the oldest.
