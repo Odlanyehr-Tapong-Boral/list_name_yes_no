54c6d288a196bd9454f04165d88d9d807ec1531d
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-25 14:43:17 +0800

    error mesage

--- listname_yes_no.py
+++ listname_yes_no.py
@@ -1,10 +1,18 @@
 #Create a program that ask user to input name and age.
-var1 = (input("Please input name "))
-var2 = (input("Please input age "))
+try:
+     var1 = (input("Please input name "))
+except:
+    print("Try again")  
+    
+try:   
+        var2 = int(input("Please input age "))
+except:
+        print("Try again")       
+    
 
-print(var1)
-print(var2)
+
 # Print error message when the input is not valid. Set your own definition of valid name and valid age.
+
 # Store all the collected information into array. After every input, ask the user if want to input another entry.
 # When “Yes”, ask the user again for input. Do it until the user respond “No”. When the user responded “No”, display the name and age of the oldest person.
 # Use the array in checking who is the oldest.
